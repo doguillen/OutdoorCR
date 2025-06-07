918e549bee53568935e814ba3ceb579ea601e7f3
doguillen <145406956+doguillen@users.noreply.github.com>
2025-06-07 14:18:05 -0600

    OutdoorCR_0.1
    Creación inicial del proyecto principal y proyecto web únicamente

---
+++ OutdoorCR.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OutdoorCR.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
