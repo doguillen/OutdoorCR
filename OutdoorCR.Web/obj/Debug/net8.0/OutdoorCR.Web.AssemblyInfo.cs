6e051d3b7efe7714ce2ea1b0b52595fb877d07ef
doguillen <145406956+doguillen@users.noreply.github.com>
2025-06-18 22:59:18 -0600

    División Layout y menu
    Se segmenta Menu de Layout usando PartialAsync

--- OutdoorCR.Web/obj/Debug/net8.0/OutdoorCR.Web.AssemblyInfo.cs
+++ OutdoorCR.Web/obj/Debug/net8.0/OutdoorCR.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OutdoorCR.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841df47573f1df4b1bd32022a01d08aeae1e3416")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed97c447874d24e3f505b3caf36f10e30d05a20")]
 [assembly: System.Reflection.AssemblyProductAttribute("OutdoorCR.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OutdoorCR.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
